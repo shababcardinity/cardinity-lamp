c90682cf0f1bc4fd0610852f2daa5f81a27f73db
shababcardinity <shabab@cardinity.com>
2020-10-28 11:30:46 +0600

    first commit

---
+++ www/opencart23/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://testas.do/opencart23/admin/');
+define('HTTP_CATALOG', 'http://testas.do/opencart23/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://testas.do/opencart23/admin/');
+define('HTTPS_CATALOG', 'http://testas.do/opencart23/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart23/admin/');
+define('DIR_SYSTEM', '/var/www/html/opencart23/system/');
+define('DIR_IMAGE', '/var/www/html/opencart23/image/');
+define('DIR_LANGUAGE', '/var/www/html/opencart23/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart23/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/opencart23/system/config/');
+define('DIR_CACHE', '/var/www/html/opencart23/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart23/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/opencart23/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/opencart23/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/opencart23/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/opencart23/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'oc_fresh_23');
+define('DB_PASSWORD', 'oc_fresh_23');
+define('DB_DATABASE', 'oc_fresh_23');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ www/opencart23/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://testas.do/opencart23/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://testas.do/opencart23/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart23/catalog/');
+define('DIR_SYSTEM', '/var/www/html/opencart23/system/');
+define('DIR_IMAGE', '/var/www/html/opencart23/image/');
+define('DIR_LANGUAGE', '/var/www/html/opencart23/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart23/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/opencart23/system/config/');
+define('DIR_CACHE', '/var/www/html/opencart23/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart23/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/opencart23/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/opencart23/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/opencart23/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'oc_fresh_23');
+define('DB_PASSWORD', 'oc_fresh_23');
+define('DB_DATABASE', 'oc_fresh_23');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
